ced6a18365e68d9398be96fefb36b9120f791db7
FlorenceSoudain <43751399+FlorenceSoudain@users.noreply.github.com>
2018-12-19 14:22:13 +0100

    V2

---
+++ essai.php
@@ -0,0 +1,19 @@
+<?php
+/**
+ * Created by PhpStorm.
+ * User: Administrateur
+ * Date: 19/12/2018
+ * Time: 14:14
+ */
+
+// fonction pour déterminer si un nombre est pair ou impair
+function pair ($num){
+    if($num % 2 === 0) {
+        return "Pair";
+    }
+    else {
+        return "Impair";
+    }
+}
+
+echo pair(7);
